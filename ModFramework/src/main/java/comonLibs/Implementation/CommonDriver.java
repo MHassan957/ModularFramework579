be881b76ae551350f014246842397e4933a9abdf
14153 <14153@DESKTOP-513MI2R>
2019-09-18 12:13:46 -0700

    commonDriver code

---
+++ ModFramework/src/main/java/comonLibs/Implementation/CommonDriver.java
@@ -0,0 +1,67 @@
+package comonLibs.Implementation;
+
+import comonLibs.contracs.IDriver;
+
+public class CommonDriver implements IDriver {
+
+	@Override
+	public void navigateToFirstUrl(String url) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getTitle() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCurrentUrl() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPageSource() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void navigateToUrl(String url) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void navigateForward() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void navigateBackward() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void refresh() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void closeBrowser() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void closeAllBrowsers() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ ModFramework/src/main/java/comonLibs/contracs/IScreenshot.java
@@ -0,0 +1,5 @@
+package comonLibs.contracs;
+
+public interface IScreenshot {
+
+}
